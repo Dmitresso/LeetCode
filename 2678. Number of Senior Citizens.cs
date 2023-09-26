fea4db1a3ae86f7fb5d8b21a378035c682aaadfd
Dmitresso <dmitresso@gmail.com>
2023-09-26 13:27:00 +0700

    2678. Number of Senior Citizens without regex

--- 2678. Number of Senior Citizens.cs
+++ 2678. Number of Senior Citizens.cs
@@ -1,9 +1,7 @@
-using System.Text.RegularExpressions;
-
 public class Solution {
     public int CountSeniors(string[] details) {
         int count = 0;
-        foreach (var s in details) if (Regex.IsMatch(s, @"[FMO]([6][1-9]|[7-9][0-9])\d{2}$")) count++;
+        foreach (var s in details) if (int.Parse(new string(new char[] { s[11], s[12] })) > 60) count++;
         return count;
     }
 }
